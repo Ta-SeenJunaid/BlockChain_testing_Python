68556e0e32ed2fca11fe2c9ddb22422b24ed2047
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-07-17 23:06:25 +0600

    Setup

--- blockchain.py
+++ blockchain.py
@@ -9,16 +9,26 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
-    return float(input('Your transaction amount please: '))
+def get_transaction_value():
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-tx_amount = get_user_input()
-add_value(transaction_amount=tx_amount, last_transaction=get_last_blockchain_value())
+def print_blockchain_elements():
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
 
-tx_amount = get_user_input()
-add_value(transaction_amount=tx_amount, last_transaction=get_last_blockchain_value())
 
-print(blockchain)
+
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+
+    else:
+        print_blockchain_elements()
+
+
+
