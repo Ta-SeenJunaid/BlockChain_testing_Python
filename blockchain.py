90ce84e4b26697d4812f15eb7db887637e48369c
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-07-21 09:43:31 +0600

    Condition is applied

--- blockchain.py
+++ blockchain.py
@@ -9,28 +9,37 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-
-tx_amount = get_user_input()
-add_value(tx_amount)
-
-
-while True:
-    tx_amount = get_user_input()
-    add_value(transaction_amount=tx_amount, last_transaction=get_last_blockchain_value())
-
+def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
 
 
+tx_amount = get_transaction_value()
+add_value(tx_amount)
 
 
+while True:
+    print('Please choose')
+    print('1: Add a new transaction')
+    print('2: Output the blockchains blocks')
 
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+
+    else:
+        print_blockchain_elements()
 
 print('Done!')
 
