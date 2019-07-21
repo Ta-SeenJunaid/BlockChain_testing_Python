094edfdedccba67890adb9d5169703f8b0f97cc6
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-07-21 09:15:59 +0600

    Loop is added

--- blockchain.py
+++ blockchain.py
@@ -9,26 +9,30 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_transaction_value():
+def get_user_input():
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
-def get_user_choice():
-    user_input = input('Your choice: ')
-    return user_input
 
-def print_blockchain_elements():
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+
+while True:
+    tx_amount = get_user_input()
+    add_value(transaction_amount=tx_amount, last_transaction=get_last_blockchain_value())
+
     for block in blockchain:
         print('Outputting Block')
         print(block)
 
 
 
-        tx_amount = get_transaction_value()
-        add_value(tx_amount, get_last_blockchain_value())
 
-    else:
-        print_blockchain_elements()
+
+
+print('Done!')
 
 
 
